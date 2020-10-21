65ec88ff2362d913af8ddd4aeabae18c64d2611d
Codyb171 <codybishop171@gmail.com>
2020-10-21 11:41:18 -0400

    Revert "Revert "Added Login Awareness to all masterpages excluding the defualt""
    This reverts commit d43eb44b695babe8ce427610824a01eee43480ed.

--- CyberDayInformationSystem/Teacher.Master.designer.cs
+++ CyberDayInformationSystem/Teacher.Master.designer.cs
@@ -15,6 +15,15 @@ namespace CyberDayInformationSystem
     {
 
         /// <summary>
+        /// UserLoggedIn control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label UserLoggedIn;
+
+        /// <summary>
         /// LogBtn control.
         /// </summary>
         /// <remarks>
