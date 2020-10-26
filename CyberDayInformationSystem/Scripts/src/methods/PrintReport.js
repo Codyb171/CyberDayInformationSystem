19cf7a23f45ab52182218f2960d2596d63c4b936
Codyb171 <codybishop171@gmail.com>
2020-10-25 20:00:38 -0400

    updated my javascript file

--- CyberDayInformationSystem/Scripts/src/methods/PrintReport.js
+++ CyberDayInformationSystem/Scripts/src/methods/PrintReport.js
@@ -3,10 +3,8 @@
     var printWindow = window.open("", "", "height=600,width=1000");
     printWindow.document.write("<html><head><title>Print Report</title>");
     printWindow.document.write("</head><body >");
-
     printWindow.document.write(panel.item(0).innerHTML);
     printWindow.document.write("</body></html>");
-
     printWindow.document.close();
     setTimeout(function() {
             printWindow.print();
