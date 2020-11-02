4f4a25950e3760a37fee98c8ea75c7c8fdf21d5e
kowahlpm <kowahlpm@dukes.jmu.edu>
2020-11-01 19:54:58 -0500

    Error with bootstrap buttons that was making program unrunable. Commented out and re-added the asp buttons until it can be fixed. - See GroupMe.

--- CyberDayInformationSystem/Site.Master.designer.cs
+++ CyberDayInformationSystem/Site.Master.designer.cs
@@ -15,6 +15,24 @@ namespace CyberDayInformationSystem
     {
 
         /// <summary>
+        /// SignUpBtn control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button SignUpBtn;
+
+        /// <summary>
+        /// LogBtn control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button LogBtn;
+
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
