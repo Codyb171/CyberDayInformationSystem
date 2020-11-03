1b0656e525bcb30dafdba65b6baac9d41f546810
Codyb171 <codybishop171@gmail.com>
2020-11-03 10:40:07 -0500

    fixed error

--- CyberDayInformationSystem/Parent.Master.cs
+++ CyberDayInformationSystem/Parent.Master.cs
@@ -11,7 +11,24 @@ namespace CyberDayInformationSystem
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (Session["User"] != null)
+            {
+                string user = Session["User"].ToString();
+                UserLoggedIn.Text = "Welcome " + user + "\t";
+                LogBtn.Text = "Logout?";
+            }
+        }
+        protected void LogBtn_Click(object sender, EventArgs e)
+        {
+            if (Session["User"] != null)
+            {
+                Session.Abandon();
+                Response.Redirect("BadSession.aspx");
+            }
+            else
+            {
+                Response.Redirect("LoginPage.aspx");
+            }
         }
     }
 }
