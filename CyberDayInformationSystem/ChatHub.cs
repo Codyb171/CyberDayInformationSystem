189b0aceb9257a82a788f2d434e3ce4bcf94a822
Sara Ghazzaoui <sghazzaoui@gmail.com>
2020-11-04 16:25:31 -0500

    Added Chatbox Functionality with emojis! Chat tab is added to the master site but this will be changed later.

---
+++ CyberDayInformationSystem/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace CyberDayInformationSystem
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
+        }
+    }
+}
---
+++ CyberDayInformationSystem/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(CyberDayInformationSystem.Startup))]
+
+namespace CyberDayInformationSystem
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+
+        }
+    }
+}
