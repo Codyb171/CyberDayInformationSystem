6dab32b2e5b938ec5f1ab91a5032ff982b278eaa
Thomas Knupp <knupptj@dukes.jmu.edu>
2020-11-08 16:06:18 -0500

    Added login awareness to HeatMap

--- CyberDayInformationSystem/HeatMap.aspx.cs
+++ CyberDayInformationSystem/HeatMap.aspx.cs
@@ -9,6 +9,25 @@ namespace CyberDayInformationSystem
 {
     public partial class HeatMap : System.Web.UI.Page
     {
+
+        void Page_PreInit(Object sender, EventArgs e)
+        {
+            if (Session["TYPE"] != null)
+            {
+                MasterPageFile = (Session["Master"].ToString());
+                if (Session["TYPE"].ToString() != "Coordinator")
+                {
+                    Session.Add("Redirected", 1);
+                    Response.Redirect("BadSession.aspx");
+                }
+            }
+            else
+            {
+                Session.Add("Redirected", 0);
+                Response.Redirect("BadSession.aspx");
+            }
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
