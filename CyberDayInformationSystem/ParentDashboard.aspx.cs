2c6b768524c69949c846028cfc90dbdf15c5d244
kowahlpm <kowahlpm@dukes.jmu.edu>
2020-11-08 17:42:50 -0500

    preint parent stuff

--- CyberDayInformationSystem/ParentDashboard.aspx.cs
+++ CyberDayInformationSystem/ParentDashboard.aspx.cs
@@ -7,6 +7,23 @@ namespace CyberDayInformationSystem
 {
     public partial class ParentDashboard : Page
     {
+        void Page_PreInit(Object sender, EventArgs e)
+        {
+            if (Session["TYPE"] != null)
+            {
+                MasterPageFile = (Session["Master"].ToString());
+                if (Session["TYPE"].ToString() != "Parent")
+                {
+                    Session.Add("Redirected", 1);
+                    Response.Redirect("BadSession.aspx");
+                }
+            }
+            else
+            {
+                Session.Add("Redirected", 0);
+                Response.Redirect("BadSession.aspx");
+            }
+        }
         protected void Page_Load(object sender, EventArgs e)
         {
 
