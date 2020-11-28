a315c63e49dc7f45a8115632649dd43f365679f7
kowahlpm <kowahlpm@dukes.jmu.edu>
2020-11-27 23:26:29 -0500

    This function is mostly working. Works if a student only has 1 note. Need to add new functionality for if a student has multiple notes to make it easier to read all the notes.

--- CyberDayInformationSystem/AdminStudentNotes.aspx
+++ CyberDayInformationSystem/AdminStudentNotes.aspx
@@ -5,10 +5,60 @@
         <li>Student Administration</li>
     </ul>
 
-    <asp:DetailsView ID="studentModDtl" runat="server" HorizontalAlign="Center"
-        AutoGenerateRows="True" DataKeyNames="FIRSTNAME" DefaultMode="ReadOnly"
-        Height="50px" Width="301px" Visible="true" AllowPaging="True" OnPageIndexChanging="StudentModifyDtl_PageIndexChanging">
-        <PagerSettings Mode="Numeric" Position="Bottom" Visible="True"/>
-    </asp:DetailsView>
+    <asp:MultiView ID="CurView" runat="server">
+        <asp:View ID="StudentDetailView" runat="server">
+            <asp:Table ID="tblSelect" runat="server" HorizontalAlign="Center">
+                <asp:TableRow>
+                    <asp:TableCell ColumnSpan="4" HorizontalAlign="Center">
+                        <asp:DetailsView ID="studentModDtl" runat="server" HorizontalAlign="Center"
+                            AutoGenerateRows="True" DataKeyNames="STUDENTID" DefaultMode="ReadOnly"
+                            Height="50px" Width="301px" Visible="true" AllowPaging="True" OnPageIndexChanging="StudentModifyDtl_PageIndexChanging">
+                            <PagerSettings Mode="Numeric" Position="Bottom" Visible="True"/>
+                        </asp:DetailsView>
+                    </asp:TableCell>
+                </asp:TableRow>
+                <asp:TableRow>
+                    <asp:TableCell ColumnSpan="2" HorizontalAlign="Center">
+                        <asp:Button ID="btnAddNotes" CssClass="btn-main reg" runat="server" Text="Add Notes" OnClick="btnAddNotes_Click" />
+                    </asp:TableCell>
+                </asp:TableRow>
+            </asp:Table>
+        </asp:View>
 
+        <asp:View ID="AddNotesView" runat="server">
+            <asp:Table ID="tblAdd" runat="server" HorizontalAlign="Center">
+                <asp:TableRow runat="server">
+                    <asp:TableCell ColumnSpan="2">
+                        <asp:Label ID="lblStuFName" runat="server" Text=""></asp:Label>
+                    </asp:TableCell>
+                    <asp:TableCell ColumnSpan="2">
+                        <asp:Label ID="lblStuLName" runat="server" Text=""></asp:Label>
+                    </asp:TableCell>
+                </asp:TableRow>
+                <asp:TableRow>
+                    <asp:TableCell ColumnSpan="2">
+                        <asp:Label ID="lblAddNote" runat="server" Text="Note: (limit 100 characters)"></asp:Label>
+                    </asp:TableCell>
+                    <asp:TableCell ColumnSpan="2">
+                        <asp:TextBox ID="txtAddNote" runat="server" Width="450px" Height="50px" MaxLength="100" TextMode="MultiLine"></asp:TextBox>
+                    </asp:TableCell>
+                </asp:TableRow>
+                <asp:TableRow>
+                    <asp:TableCell ColumnSpan="4" HorizontalAlign="Center">
+                        <asp:Button ID="btnSubmit" CssClass="btn-main reg" runat="server" Text="Save Note" OnClick="btnSubmit_Click" />
+                    </asp:TableCell>
+                </asp:TableRow>
+                <asp:TableRow>
+                    <asp:TableCell ColumnSpan="4" HorizontalAlign="Center">
+                        <asp:Button ID="btnBack" CssClass="btn-main reg" runat="server" Text="Select New" OnClick="btnBack_Click" />
+                    </asp:TableCell>
+                </asp:TableRow>
+                <asp:TableRow>
+                    <asp:TableCell ColumnSpan="4" HorizontalAlign="Center">
+                        <asp:Label ID="lblStatus" runat="server" Text="" ForeColor="Red"></asp:Label>
+                    </asp:TableCell>
+                </asp:TableRow>
+            </asp:Table>
+        </asp:View>
+    </asp:MultiView>
 </asp:Content>
--- CyberDayInformationSystem/AdminStudentNotes.aspx.designer.cs
+++ CyberDayInformationSystem/AdminStudentNotes.aspx.designer.cs
@@ -15,6 +15,33 @@ namespace CyberDayInformationSystem
     {
 
         /// <summary>
+        /// CurView control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.MultiView CurView;
+
+        /// <summary>
+        /// StudentDetailView control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.View StudentDetailView;
+
+        /// <summary>
+        /// tblSelect control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Table tblSelect;
+
+        /// <summary>
         /// studentModDtl control.
         /// </summary>
         /// <remarks>
@@ -22,5 +49,95 @@ namespace CyberDayInformationSystem
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.DetailsView studentModDtl;
+
+        /// <summary>
+        /// btnAddNotes control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAddNotes;
+
+        /// <summary>
+        /// AddNotesView control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.View AddNotesView;
+
+        /// <summary>
+        /// tblAdd control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Table tblAdd;
+
+        /// <summary>
+        /// lblStuFName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblStuFName;
+
+        /// <summary>
+        /// lblStuLName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblStuLName;
+
+        /// <summary>
+        /// lblAddNote control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblAddNote;
+
+        /// <summary>
+        /// txtAddNote control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtAddNote;
+
+        /// <summary>
+        /// btnSubmit control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnSubmit;
+
+        /// <summary>
+        /// btnBack control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnBack;
+
+        /// <summary>
+        /// lblStatus control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblStatus;
     }
 }
